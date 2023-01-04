508b80dace1e0054e93cceb9aac9ca645c44a78e
Renat Prokopiev <prokop4891@gmail.com>
2023-01-04 15:41:05 +0300

    2nd task update

--- 003_2_SumOfOppositePairsInList/2_SumOfOppositePairsInList.py
+++ 003_2_SumOfOppositePairsInList/2_SumOfOppositePairsInList.py
@@ -1,9 +1,6 @@
 # Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-# print(5%2)
-# exit()
-
 def sumListNumbersOppositeIndx(lst):
     lst_sum = []
     y = 1
@@ -11,17 +8,15 @@ def sumListNumbersOppositeIndx(lst):
         for i in range(0, ((len(lst)//2)+1)):
             lst_sum.insert(i, (lst[i]*lst[len(lst)-y]))
             y += 1
-            print(lst_sum)
         return lst_sum
     else:
         for i in range(0, ((len(lst)//2))):
             lst_sum.insert(i, (lst[i]*lst[len(lst)-y]))
             y += 1
-            print(lst_sum)
         return lst_sum
 
 
-list = [5, 1, 3, 3, 6]
+list = [5, 1, 3, 13, 6]
 print(list)
 list_sums = sumListNumbersOppositeIndx(list)
 print(list_sums)
