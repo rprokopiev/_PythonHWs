7b97122a255ffdcab647d4a135a6c9ad2327bfeb
Renat Prokopiev <prokop4891@gmail.com>
2023-01-09 22:05:17 +0300

    2nd of 4th

--- 004_2_GetNSimpleMult/2_GetNSimpleMult.py
+++ 004_2_GetNSimpleMult/2_GetNSimpleMult.py
@@ -1,3 +1,14 @@
-# Задайте натуральное число N. 
+# Задайте натуральное число N.
 # Напишите программу, которая составит список простых множителей числа N.
 # e.g. 12 => 2, 2, 3. простое число - делится только на себя и один.
+import math
+n = int(input('Введите число \n'))
+x = n
+primeIntList = []
+for i in range(2, (int(n/2))):
+    while x % i == 0:
+        x //= i
+        primeIntList.append(i)
+else:
+    primeIntList.append(n)
+print(primeIntList)
