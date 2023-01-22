c6c3e9716c10b3bebc75671f4bd39063ca6f29fe
Renat Prokopiev <prokop4891@gmail.com>
2023-01-22 23:05:45 +0300

    6th verification

--- 006_2_SumOfOddIndxNumb/2_SumOfOddIndxNumb.py
+++ 006_2_SumOfOddIndxNumb/2_SumOfOddIndxNumb.py
@@ -1,12 +1,23 @@
 # Найти сумму чисел списка стоящих на нечетной позиции
+import random
+a = [random.randint(1, 10) for i in range(6)]
+res = sum([value for indx, value in enumerate(a) if indx % 2 == 1])
+print(a)
+print(res)
+
+exit()
+# MINE
+
 def GetRandList(x, y):
     import random
     lst = [random.randint(1, x) for i in range(0, y)]
     return lst
 
+
 lst = GetRandList(int(input('max value \n')), int(input('list lenght \n')))
 print(lst)
 oddIndNumbSum = 0
 for i in range(0, len(lst)):
-    if i % 2 == 1: oddIndNumbSum += lst[i]
+    if i % 2 == 1:
+        oddIndNumbSum += lst[i]
 print(oddIndNumbSum)
--- 006_3_OppositeNumbSum/3_OppositeNumbSum.py
+++ 006_3_OppositeNumbSum/3_OppositeNumbSum.py
@@ -1,5 +1,15 @@
 # Найти произведение пар чисел списка(парой считаем первый и последний, второй предпоследний и тд)
 
+import random
+a = [random.randint(1, 10) for i in range(5)]
+res = [a[indx]*a[-indx-1] for indx in range(len(a)//2+len(a) % 2)]
+print(a)
+print(res)
+
+exit()
+# MINE
+
+
 def GetRandList(x, y):
     import random
     lst = [random.randint(1, x) for i in range(0, y)]
@@ -7,15 +17,15 @@ def GetRandList(x, y):
 
 
 lst1st = GetRandList(int(input('numbers max value - ')),
-                        int(input('list lenght - ')))
+                     int(input('list lenght - ')))
 print(lst1st)
 
 lst2nd = []
-if len(lst1st)%2 == 1:
+if len(lst1st) % 2 == 1:
     for i in range(0, (len(lst1st)//2) + 1):
         lst2nd.append(lst1st[i]*lst1st[((len(lst1st))-1)-len(lst2nd)])
 else:
     for i in range(0, (len(lst1st)//2)):
         lst2nd.append(lst1st[i]*lst1st[((len(lst1st))-1)-len(lst2nd)])
-    
+
 print(lst2nd)
